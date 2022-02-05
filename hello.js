a5b7505360f267544c3215727ec83436762de9b9
Dave McKinnon <dave.w.mckinnon@gmail.com>
2022-02-05 20:31:50 +0000

    basic function examples

---
+++ hello.js
@@ -0,0 +1,15 @@
+// First Example
+/*const sayHello  = function () {
+  console.log("Hello, world");
+}
+
+sayHello();*/
+
+// Add name parameter
+const sayHello  = function (name) {
+  console.log("Hello, " + name);
+}
+
+sayHello("Caliban");
+sayHello("Miranda");
+sayHello("Ferdinand");
