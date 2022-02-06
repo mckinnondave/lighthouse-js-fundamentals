5186d5c959b71bf61d79de046af44b875d17eeca
Dave McKinnon <dave.w.mckinnon@gmail.com>
2022-02-06 20:46:09 +0000

    function for creating an array with set ranges

---
+++ looping-range.js
@@ -0,0 +1,18 @@
+function range(start, end, step) {
+  let arr = [];
+  if (start === undefined || end === undefined || step === undefined) {
+    return arr;
+  } else if (start > end) {
+    return arr;
+  } else if (step <= 0) {
+    return arr;
+  } else {  
+    for (let i = start; i <= end; i += step) {
+      arr.push(i);
+    }
+    return arr; 
+  }
+}
+console.log(range(0, 10, 2));
+console.log(range(10, 30, 5));
+console.log(range(-5, 2, 3));
