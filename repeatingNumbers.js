6bb1de41107f081a4b90259de65dbfbffc785b62
Dave McKinnon <dave.w.mckinnon@gmail.com>
2022-02-12 22:52:49 +0000

    function to repeat numbers based off values in array

---
+++ repeatingNumbers.js
@@ -0,0 +1,18 @@
+function repeatNumbers (data) {
+  let output = "";
+  for (let i = 0; i < data.length; i++) {
+    for (let j = 0; j < data[i][1]; j++) {
+      output += data[i][0];
+    }
+    if (i < data.length - 1) {
+      output += ", "
+    }
+  }
+  return output;
+}
+     
+
+
+console.log(repeatNumbers([[1, 10]]));
+console.log(repeatNumbers([[1, 2], [2, 3]]));
+console.log(repeatNumbers([[10, 4], [34, 6], [92, 2]]));
